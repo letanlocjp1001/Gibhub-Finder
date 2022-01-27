c023729b8aec3bdc62aff3a72a08432ca17b4130
LE TAN LOC <letanlocjp1001@gmail.com>
2022-01-28 08:41:55 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-3xl font-bold underline yellow'>Hello world!</h1>
+      <div class='flex justify-between w-screen'>
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+      <button className='btn btn-primary'>Click</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
