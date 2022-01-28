e50f0972cf775155f5793548ec8eed6840caf2cc
LE TAN LOC <letanlocjp1001@gmail.com>
2022-01-28 10:20:10 +0900

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline yellow'>Hello world!</h1>
-      <div class='flex justify-between w-screen'>
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <button className='btn btn-primary'>Click</button>
-    </div>
+    </Router>
   )
 }
 
