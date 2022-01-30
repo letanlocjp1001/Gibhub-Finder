8c8282b542b0bc999833337d051073736e4d8e76
LE TAN LOC <letanlocjp1001@gmail.com>
2022-01-31 08:21:46 +0900

    first

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -6,7 +6,9 @@ const RepoList = ({ repos }) => {
     <div className='rounded-lg shadow-lg card bg-base-100'>
       <div className='card-body'>
         <h2 className='text-3xl my-4 font-bold card-title'>
-          Top Respositories
+          {repos.map((repo) => (
+            <h3>{repo.name}</h3>
+          ))}
         </h2>
       </div>
     </div>
